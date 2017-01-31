970dd3320c553b4059014b0110fb1fa6c07a6728
jamatan1 <james.matan@sonomaacademy.org>
2017-01-31 14:35:50 -0800

    added new project

--- gradient/sketch.js
+++ gradient/sketch.js
@@ -3,16 +3,22 @@ var x = 1;
 var r = 255;
 var g = 0;
 var b = 0;
-var diag = (Math.sqrt(width**2 + height**2));
+var diag;
 function setup() {
   background("white");
   createCanvas(100, 100);
+  diag = (Math.sqrt(width**2 + height**2));
+}
+function draw(){
   for(i = 0; i < width; i++){
     var col = color(r, g, b);
     set([i, 100], loops, col);
     updatePixels();
-    loops = loops + x;
-      r = r - (255/diag);
-      b = b + (255/diag);
 }
+loops = loops + x;
+  r = r - (255/(diag*10));
+  b = b + (255/(diag*10));
+  if(loops === 100){
+    noLoop();
+  }
 }
