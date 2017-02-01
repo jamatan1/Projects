e46e225e5c8244bf810256d147db5ff16d508443
jamatan1 <james.matan@sonomaacademy.org>
2017-02-01 08:47:05 -0800

    made video rotate and added rotating text

--- animate/sketch.js
+++ animate/sketch.js
@@ -1,4 +1,5 @@
 var video;
+var rads = 0;
 function setup() {
   canvas = createCanvas(640, 480);
   video = createCapture(VIDEO);
@@ -7,5 +8,20 @@ function setup() {
 
 function draw() {
   background('white');
+  push();
+  translate(width/2, height/2);
+  rotate(rads);
+  imageMode(CENTER);
   image(video, 0, 0, width, height);
+  rotate(rads);
+  pop();
+  push();
+  translate(width/4, height*.75);
+  rotate(rads);
+  textSize(20);
+  fill('hotPink');
+  text('You spin me right round baby right round', -100, 20);
+  pop();
+  rads = rads + .02;
+
 }
