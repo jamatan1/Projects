324a41aec1c3062dc5bbaf217dd0d566d265da9d
jamatan1 <james.matan@sonomaacademy.org>
2017-02-02 14:55:39 -0800

    yeet

--- animate/sketch.js
+++ animate/sketch.js
@@ -6,6 +6,7 @@ var photos;
 function setup() {
   colorMode(HSB);
   createCanvas(500, 500);
+
   video = createCapture(VIDEO);
   video.hide();
   photos = [];
@@ -13,22 +14,10 @@ function setup() {
 }
 
 function draw() {
-  background("white");
   if (frameCount % 30 === 0){
-    var photo = {};
-
-    photo.img = video.get();
-    photo.tint = color(floor(random(256)), random(256), random(256));
-
-    photos[i % 70] = photo;
+    photos[i] = video.get();
+    tint(color(floor(random(255)), 50, 100));
+    image(video, ((i % 25 ) % 5) * 70, floor((i % 25) / 5) * 70, 70, 70);
     i++;
-  }
-
-  for (var l = 0; l < photos.length; l++){
-    push();
-    tint(photos[l].tint);
-    image(photos[l].img, (l % 5) * 70, floor(l / 5) * 70, 70, 70);
-    noTint();
-    pop();
   }
 }
